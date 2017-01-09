cd86a2f7181dbf359cd6d48fedb3ee9c9ad57569
Emily Mendez <emilypmendez@gmail.com>
2017-01-09 10:36:35 -0600

    Updated notes upon each exec

--- main.lua
+++ main.lua
@@ -1,48 +1,49 @@
 -----------------------------------------------------------------------------------------
 --
--- main.lua
+-- The main.lua file built from my pseudo instructions (in ze "code" journal at me house)
 --
 -----------------------------------------------------------------------------------------
 
--- Your code here
-
+-- 1 -- loading the screen background and positioning --
 local background = display.newImageRect ( "background.png", 360, 570)
 background.x = display.contentCenterX
 background.y = display.contentCenterY
--- loading the screen background and positioning --
+
+-- 2 -- event for displaying number of taps on the balloon --
 local tapCount = 0
 local tapText = display.newText( tapCount, display.contentCenterX, 20, native.system, 40)
 
--- event for displaying number of taps on the balloon --
+-- 3 -- loading the platform --
 
 local platform = display.newImageRect( "platform.png", 300, 50 )
 platform.x = display.contentCenterX
 platform.y = display.contentHeight-25
--- loading the platform --
+-- 4 -- adjusting balloon locale on screen and balloon opacity --
 
 local balloon = display.newImageRect ("balloon.png", 112, 112 )
 balloon.x = display.contentCenterX
 balloon.y = display.contentCenterY
 balloon.alpha = 0.8
--- adjusting balloon locale on screen and balloon opacity -- 
 
+-- 5 -- load physics into game --
 local physics = require ("physics")
+
+-- 6 -- enable the platform as static for other objects to remain static
 physics.start()
 
 physics.addBody( platform, "static" )
-physics.addBody( balloon, "dynamic", { radius=50, bounce=0.3 } )
--- load physics into the game,
--- enable the platform as static for other objects to remain static
--- then enable the balloon to become dynamic (can change bounce rate)
 
+-- 7 -- then enable the balloon to become dynamic (can change bounce rate)
+physics.addBody( balloon, "dynamic", { radius=50, bounce=0.3 } )
+
+-- 8 -- allow the balloon to be pushed each time it is tapped
+-- addon functionality -- increase the variable by 1 each time balloon is tapped
+-- addon functionality-- updates text property of the 'tapText' object
 local function pushBalloon()
 	balloon:applyLinearImpulse( 0, -0.75, balloon.x, balloon.y )
 	tapCount = tapCount + 1
 	tapText.text = tapCount
 end
--- line 35 = allow the balloon to be pushed each time it is tapped
--- line 36 = increase the variable by 1 each time balloon is tapped
--- line 37 = updates text property of the 'tapText' object
 
+-- 9 -- trigger event = tapping balloon
 balloon:addEventListener( "tap", pushBalloon )
--- trigger event = tapping balloon
